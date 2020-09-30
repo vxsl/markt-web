708f7d200795eb1e2d56ccc07fc834a9b0fcb86e
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-09-30 10:42:38 -0400

    now working to update regularly. Using callbacks to print formatted updates only when gainer list changes. Improved encapsulation

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const scraper = require("./bnnbloomberg-markets-scraper")
 
-var cur
-var prev
 
 var market = []
 var timestamp
@@ -17,43 +15,86 @@ const getStockBySymbol = (symbol) => {
 const display = () => {
 
 	for (var i = 0; i < 10; i++) {
-		console.log(market[i])
+		console.log(market[i].symbol)
 	}		
-	console.log(timestamp)
+	//console.log(timestamp)
 }
 
 const init = async () => {
 
-	cur = await scraper.gainers()
-	market = [...cur.data.stocks]
-	market.sort((a, b) => b.pctChng - a.pctChng)
+	var tmp = await scraper.gainers()
+	market = [...tmp.data.stocks]
+	sortStocks(market)
+	timestamp = tmp.generatedTimestamp
+}
+
+const marketDiff = (stockArr) => {
+
+	//stockArr.map((stock, newIndex) => stockDiff(stock, newIndex))
+
+	var oldIndex
+	for (var newIndex = 0; newIndex < stockArr.length; newIndex++) {
+
+		oldIndex = market.findIndex(s  => s.symbol == stockArr[newIndex].symbol); 
+		if (newIndex != oldIndex) {
+			
+			return true
+			/*process.stdout.write(stock.symbol)
+			var a = (newIndex - oldIndex > 0) ? "jumped" : "fell"
+			process.stdout.write(a +" from " + oldIndex + " to " + newIndex + "...\n")*/			
+		}
+	}
+	return false
+}
+
+/*
+const stockDiff	= (stock, newIndex) => {
+	//console.log("stockDiff(" +stock.symbol +")")
+	var oldIndex = market.findIndex(s  => s.symbol == stock.symbol); 
+	//console.log("old index of " + stock.symbol + " is " + oldIndex )
+	//console.log("new index of " + stock.symbol + " is " + newIndex + "\n\n")
+	if (newIndex != oldIndex) {
+		process.stdout.write(stock.symbol)
+		var a = (newIndex - oldIndex > 0) ? "jumped" : "fell"
+		process.stdout.write(a +" from " + oldIndex + " to " + newIndex + "...\n")
+	}	                
+}*/
+
+const sortStocks = (stockArr) => {
+
+	stockArr.sort((a, b) => b.pctChng - a.pctChng);
+	return stockArr
+}
+
+const updateMarket = (cur) => {
+
 	timestamp = cur.generatedTimestamp
-	
-	display()
+	if (marketDiff(sortStocks(cur.data.stocks))) {
+		market = cur.data.stocks
+		console.log("*********************************************************")
+		console.log("Market updated at " + timestamp.substring(11) + ": ")
+		display()
+	}
 }
 
-const sortCur = () => {
+const poll = async (updateMarket) => {
 
-	cur.data.stocks.sort((a, b) => b.pctChng - a.pctChng);
-}
-
-const poll = async () => {
-
-	cur = await scraper.gainers()	// allows error-free first iteration
-	cur.data.stocks.sort((a, b) => b.pctChng - a.pctChng);
-
-	while(true) {
-		prev = cur
-		cur = await scraper.gainers()
-		//cur.data.stocks.sort((a, b) => b.pctChng - a.pctChng);		
-		await new Promise(r => setTimeout(r, 1000));	// sleep
-	}	
+	cur = await scraper.gainers()
+	updateMarket(cur)		
+	await new Promise(r => setTimeout(r, 1000));	// sleep
 }
 
 
 const main = async () => {
 	
-	init()
+	console.log("Initializing...")
+	await init()	
+	console.log("Market model initialized with " + market.length + " stocks.\n")
+	
+	while (true) {
+		//console.log('here')
+		await poll(updateMarket)
+	}
 }
 
 main()
