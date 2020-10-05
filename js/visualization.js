dc68b2a4d7fd4e38087fb807dba9141e3b01f9bd
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-10-05 10:43:39 -0400

    submodule scalping-agent

---
+++ js/visualization.js
@@ -0,0 +1,29 @@
+const NUM_TABLES = 6
+
+const gainersTable = document.getElementById('gainersTable')
+var gainerCharts = []
+
+for (let i = 0; i < NUM_TABLES; i++) {
+
+    gainersTable.appendChild(document.createElement('div')).innerHTML = i
+}
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
