c902c17dde8e7b6fd4cf49f7f564b6228d54e99a
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-10-06 10:41:26 -0400

    charts now displaying a responsive grid format, ready to link to market data

--- js/visualization.js
+++ js/visualization.js
@@ -1,29 +1,35 @@
 const NUM_TABLES = 6
 
-const gainersTable = document.getElementById('gainersTable')
 var gainerCharts = []
 
+var container = document.querySelector('.chartTable');
+
 for (let i = 0; i < NUM_TABLES; i++) {
+    var div = document.createElement('div');
+    div.classList.add('chart-container');
 
-    gainersTable.appendChild(document.createElement('div')).innerHTML = i
-}
+    var canvas = document.createElement('canvas');
+    div.appendChild(canvas);
+    container.appendChild(div);
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+    var ctx = canvas.getContext('2d');
+    //var config = createConfig(details.mode, details.intersect);
+    new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
 
-    // Configuration options go here
-    options: {}
-});
+        // Configuration options go here
+        options: {}
+    }); 
+};
