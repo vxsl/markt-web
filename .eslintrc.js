155ae33795b2422cdd2cd15e30b4e4a64649ffea
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2021-01-11 10:49:26 -0500

    eslint option

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
+    'vue/require-v-for-key': 'off',
   }
 }
